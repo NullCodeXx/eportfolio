c9834f6b87e094406e319a5967b94d5df691d1c6
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-04 17:10:10 +0200

    save working

--- index.php
+++ index.php
@@ -4,46 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
+    <link rel="stylesheet" href="./style.css">
+    <script src="./script.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    <title>ePortfolio Djaâfar BOUMAZA</title>
+    <title>Djaâfar BOUMAZA || ePortfolio </title>
 </head>
+
 <body>
-<img src="wallindex.jpg" alt="wallpaper street">
-    <main class="mainNav">
-        <section class="sectionNav">
-            <div class="divNav">
-                <h1 class="h1Nav">Djaafar BOUMAZA</h1>
-                <ul class="ulNav">
-                    <li class="liNav"><a href="#" class="aNav"><button type="submit">Index</button></a></li>
-                    <li class="liNav"><a href="#" class="aNav">GitHub</a></li>
-                    <li class="liNav"><a href="#" class="aNav">Serveur</a></li>
-                    <li class="liNav"><a href="#" class="aNav">Contact</a></li>
-                </ul>
+    <header>
+        <section id="section-header">
+            <div>
+                <img src="./content/logos/user.svg" alt="Logo user man" id="logo-user-header">
+                <img src="https://media-private.canva.com/MACfFWHgXtM/1/screen.jpg?response-expires=Mon%2C%2004%20Sep%202017%2017%3A07%3A55%20GMT&AWSAccessKeyId=AKIAJWF6QO3UH4PAAJ6Q&Expires=1504544875&Signature=1z6WAUrkcfpHLtDccjilrzAm3oE%3D" alt="Photo de Djaâfar BOUMAZA" id="photo-djaafar-header">
+                <h1 class="h1-header">Djaâfar BOUMAZA</h1>
+                <p class="p-header">Développeur Web junior full stack</p>
             </div>
         </section>
-    </main>
-
-    <!--VOLET DEROULANT -->
-    <section class="main">
-        <h2 class="insidemain">INDEX ALL</h2>
-        <hr class="insidemain" style="width: 80%;">
-    </section>
-</html>
+    </header>
 </body>
-<?php
-/* 
- * Add project in directory.
- */
 
-$scan = scandir("./projects");
-foreach($scan as $directory) {
-    if($directory) {
-        echo "<ul>";
-        echo "<li><button>$directory</button></li>";
-        echo "</ul>";
-    }
-}
-?>
+</html>
