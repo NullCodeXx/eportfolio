db9ad33ed4bb9e2889ddf97ea978b6b5ea0fc5e6
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-11 23:46:25 +0200

    header base poser

--- index.php
+++ index.php
@@ -6,21 +6,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/bootstrap4/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="icon" type="image/png" href="content/logos/db.png" />
+    <!-- Materialize 
+      <link rel="stylesheet" type="text/css" media="screen,projection" href="css/materialize/css/materialize.min.css" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> 
+    -->
     <title>Djaâfar BOUMAZA || ePortfolio </title>
 </head>
 
 <body>
-    <header>
-        <div class="container-fluid black">
-            <div class="container lime">
-                <div class="col-md-4 black">
-                    <img src="./content/logos/user.svg" alt="Logo utilisateur">
-                </div>
-            </div>
+    <header class="row container-fluid minightBlue">
+        <div class="col-md-9 col-xs-3 bloc-header">
+          <img id="logo-profile" src="./content/logos/user.svg" alt="Photo utilisateur">
+          <a href="index.php" title="Accueil" class="photo-lien"><img id="photo-profile" src="./content/photo/photo-reseaux.png" alt="Logo utilisateur"></a>
+          <h1 class="name">Djaafar BOUMAZA</h1>
+          <p class="flow-text functionJob">Développeur Web junior Full Stack</p>
+        </div>
+        <div class="col-md-3 bloc-header-logo">
+          <a href="https://github.com/djaafarcode" title="Voir mes projets Github"><img id="logos-reseaux1" src="./content/logos/github.svg" alt="Logo Github"></a>
+          <a href="https://plus.google.com/102441240849299749508" title="Visitez mon compte Google Plus"><img id="logos-reseaux2" src="./content/logos/google-plus.svg" alt="Logo Google Plus"></a>
+          <a href="https://www.linkedin.com/in/djaafar-boumaza-84a867146/" title="Voir mon profil Linkedin"><img id="logos-reseaux3" src="./content/logos/linkedin.svg" alt="Logo Linkedin"></a>
+          <a href="https://twitter.com/djaafarcode?lang=fr" title="Visitez mon twitter"><img id="logos-reseaux4" src="./content/logos/twitter.svg" alt="Logo Twitter"></a>
+          <a href="mailto:code-db_@outlook.fr" title="Me contacter"><img id="logos-reseaux5" src="./content/logos/mail.svg" alt="Logo E-Mail"></a>
         </div>
     </header>
-
-    <script src="./script.js"></script>
 </body>
 
 </html>
