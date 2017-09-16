fac8ef008fb207b2e5bb9273f6daca82cd653ea8
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-09-16 13:39:17 +0200

    test not function mkdir

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ $projects = [];
 
 foreach($all as $f) {
   //Vérifie si le dossier projects existe sinon création du dossier.
-  if(!mkdir('./projects', 753, true)) { //Permet la création de répertoires imbriqués spécifiés dans le pathname
-    die("Error, echec de la création du dossier.");
-  }
+  // if(!mkdir('./projects', 753, true)) { //Permet la création de répertoires imbriqués spécifiés dans le pathname
+  //   die("Error, echec de la création du dossier.");
+  // }
   //test si point ignore , fichier cacher.
   if($f[0] === ".") {
     continue;
